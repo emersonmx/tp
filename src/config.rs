fedebe1d6b0f0df060462fd578ab330c2758d046
Emerson MX <emersonmx@gmail.com>
2026-06-01 14:00:20 -0300

    test: update config coverage

--- src/config.rs
+++ src/config.rs
@@ -1,14 +1,51 @@
+#[cfg(test)]
+use mockall::automock;
 use serde::{Deserialize, Serialize};
 use std::{env, fs, io, path::PathBuf};
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error("unable to load: {0}")]
-    UnableToLoad(#[from] io::Error),
-    #[error("parser error: {0}")]
-    UnableToParseConfig(#[from] serde_yaml::Error),
     #[error("invalid session directory")]
     InvalidSessionDirectory,
+    #[error("file '{file}' not found")]
+    FileNotFound {
+        file: PathBuf,
+        #[source]
+        error: io::Error,
+    },
+    #[error("unable to read file '{file}'")]
+    FileUnreadable {
+        file: PathBuf,
+        #[source]
+        error: io::Error,
+    },
+    #[error("unable to write session file '{file}'")]
+    WriteFailed {
+        file: PathBuf,
+        #[source]
+        error: io::Error,
+    },
+    #[error("parser error")]
+    UnableToParseConfig(#[source] serde_yaml::Error),
+    #[error("unable to serialize session")]
+    SerializationFailed {
+        file: PathBuf,
+        #[source]
+        error: serde_yaml::Error,
+    },
+}
+
+#[cfg_attr(test, automock)]
+trait HomeDirProvider: Send + Sync {
+    fn home_dir(&self) -> Option<PathBuf>;
+}
+
+struct EnvHomeDirProvider;
+
+impl HomeDirProvider for EnvHomeDirProvider {
+    fn home_dir(&self) -> Option<PathBuf> {
+        env::home_dir()
+    }
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
@@ -57,27 +94,41 @@ impl Session {
     const DEFAULT_DIR_ENV: &str = "TP_SESSIONS_DIR";
     const DEFAULT_DIR: &str = ".config/tp";
     const DEFAULT_FILE_EXT: &str = "yaml";
+    const DEFAULT_HOME_DIR_PROVIDER: EnvHomeDirProvider = EnvHomeDirProvider;
 
     pub fn load_from_name(name: impl AsRef<str>) -> Result<Self, Error> {
-        let dir = Self::default_directory().ok_or(Error::InvalidSessionDirectory)?;
-
-        let path = dir
-            .join(format!("{}.{}", name.as_ref(), Self::DEFAULT_FILE_EXT))
-            .canonicalize()?;
-        let content = fs::read_to_string(path)?;
+        let session_path = Self::get_session_path(name.as_ref())?;
+        let content = fs::read_to_string(&session_path).map_err(|e| Error::FileUnreadable {
+            file: session_path,
+            error: e,
+        })?;
         let session = Self::load_from_string(&content)?;
         Ok(session)
     }
 
-    fn default_directory() -> Option<PathBuf> {
+    fn get_session_path(name: &str) -> Result<PathBuf, Error> {
+        let dir = Self::default_directory_with_provider(Self::DEFAULT_HOME_DIR_PROVIDER)
+            .ok_or(Error::InvalidSessionDirectory)?;
+        let path = dir
+            .join(format!("{}.{}", name, Self::DEFAULT_FILE_EXT))
+            .canonicalize()
+            .map_err(|e| Error::FileNotFound {
+                file: dir.join(format!("{}.{}", name, Self::DEFAULT_FILE_EXT)),
+                error: e,
+            })?;
+        Ok(path)
+    }
+
+    fn default_directory_with_provider(provider: impl HomeDirProvider) -> Option<PathBuf> {
         env::var(Self::DEFAULT_DIR_ENV)
             .ok()
             .map(PathBuf::from)
-            .or_else(|| env::home_dir().map(|home| home.join(Self::DEFAULT_DIR)))
+            .or_else(|| provider.home_dir().map(|home| home.join(Self::DEFAULT_DIR)))
     }
 
     pub fn load_from_string(content: impl AsRef<str>) -> Result<Self, Error> {
-        let session: Self = serde_yaml::from_str(content.as_ref())?;
+        let session: Self =
+            serde_yaml::from_str(content.as_ref()).map_err(Error::UnableToParseConfig)?;
         Ok(session)
     }
 
@@ -96,30 +147,39 @@ impl Session {
             }],
         };
 
-        let dir = Self::default_directory().ok_or(Error::InvalidSessionDirectory)?;
-        let path = dir.join(format!("{}.{}", session.name, Self::DEFAULT_FILE_EXT));
-        let content = serde_yaml::to_string(&session)?;
+        let dir = Self::default_directory_with_provider(Self::DEFAULT_HOME_DIR_PROVIDER)
+            .ok_or(Error::InvalidSessionDirectory)?;
+        let session_path = dir.join(format!("{}.{}", session.name, Self::DEFAULT_FILE_EXT));
 
-        fs::write(&path, content)?;
+        let content = serde_yaml::to_string(&session).map_err(|e| Error::SerializationFailed {
+            file: session_path.clone(),
+            error: e,
+        })?;
 
-        Ok(path)
+        fs::write(&session_path, content).map_err(|e| Error::WriteFailed {
+            file: session_path.clone(),
+            error: e,
+        })?;
+
+        Ok(session_path)
     }
 
     pub fn list() -> Vec<String> {
-        let mut sessions: Vec<String> = Self::default_directory()
-            .and_then(|dir| fs::read_dir(dir).ok())
-            .into_iter()
-            .flatten()
-            .filter_map(|entry_result| entry_result.ok())
-            .map(|entry| entry.path())
-            .filter(|path| path.is_file())
-            .filter(|path| path.extension().is_some_and(|ext| ext == "yaml"))
-            .filter_map(|path| {
-                path.file_stem()
-                    .and_then(|stem| stem.to_str())
-                    .map(|s| s.to_string())
-            })
-            .collect();
+        let mut sessions: Vec<String> =
+            Self::default_directory_with_provider(Session::DEFAULT_HOME_DIR_PROVIDER)
+                .and_then(|dir| fs::read_dir(dir).ok())
+                .into_iter()
+                .flatten()
+                .filter_map(|entry_result| entry_result.ok())
+                .map(|entry| entry.path())
+                .filter(|path| path.is_file())
+                .filter(|path| path.extension().is_some_and(|ext| ext == "yaml"))
+                .filter_map(|path| {
+                    path.file_stem()
+                        .and_then(|stem| stem.to_str())
+                        .map(|s| s.to_string())
+                })
+                .collect();
         sessions.sort();
         sessions
     }
@@ -129,34 +189,90 @@ impl Session {
 mod tests {
     use super::*;
     use rstest::rstest;
+    use std::{
+        fs::{File, Permissions},
+        os::unix::fs::PermissionsExt,
+    };
     use tempfile::tempdir;
 
     #[rstest]
-    fn load_from_name_unable_to_load() {
-        let session = Session::load_from_name("not-found/path");
+    fn load_from_name_file_unreadable() {
+        let session_name = "unreadable-session";
+        let temp_test_dir = tempdir().expect("Failed to create temporary directory");
+        let tmp_dir = temp_test_dir.path();
 
-        assert!(matches!(session, Err(Error::UnableToLoad(_))));
+        temp_env::with_var(
+            Session::DEFAULT_DIR_ENV,
+            Some(tmp_dir.to_str().unwrap()),
+            || {
+                let file_path = tmp_dir.join(format!("{}.yaml", session_name));
+                File::create(&file_path).unwrap();
+                fs::set_permissions(&file_path, Permissions::from_mode(0o000)).unwrap();
+
+                let result = Session::load_from_name(session_name).unwrap_err();
+
+                assert!(matches!(result, Error::FileUnreadable { .. }));
+            },
+        );
     }
 
     #[rstest]
-    fn load_from_name_invalid_dir() {
-        temp_env::with_var(Session::DEFAULT_DIR_ENV, Some("invalid-dir"), || {
-            let session = Session::load_from_name("a-session-path");
+    fn should_load_from_name() {
+        let session_name = "test-session";
+        let temp_test_dir = tempdir().expect("Failed to create temporary directory");
+        let tmp_dir = temp_test_dir.path();
 
-            assert!(matches!(session, Err(Error::UnableToLoad(_))));
+        temp_env::with_var(
+            Session::DEFAULT_DIR_ENV,
+            Some(tmp_dir.to_str().unwrap()),
+            || {
+                let file_path = tmp_dir.join(format!("{}.yaml", session_name));
+                fs::write(&file_path, "name: test-session").unwrap();
+
+                let result = Session::load_from_name(session_name).unwrap();
+                assert_eq!(result.name, session_name);
+            },
+        );
+    }
+
+    #[rstest]
+    fn get_session_path_file_not_found() {
+        temp_env::with_var_unset("HOME", || {
+            let session = Session::get_session_path("a-session-path");
+
+            assert!(matches!(session, Err(Error::FileNotFound { .. })));
+        });
+    }
+
+    #[rstest]
+    fn default_directory_when_no_envs() {
+        let mut mock = MockHomeDirProvider::new();
+        mock.expect_home_dir().return_const(None);
+
+        temp_env::with_var_unset(Session::DEFAULT_DIR_ENV, || {
+            let dir = Session::default_directory_with_provider(mock);
+            assert!(dir.is_none());
+        });
+    }
+
+    #[rstest]
+    fn default_directory_from_env() {
+        temp_env::with_var(Session::DEFAULT_DIR_ENV, Some("/custom/dir"), || {
+            let dir = Session::default_directory_with_provider(Session::DEFAULT_HOME_DIR_PROVIDER);
+            assert_eq!(dir, Some(PathBuf::from("/custom/dir")));
         });
     }
 
     #[rstest]
     fn load_from_string_parser_error() {
         let content = "parser error";
-        let session: Result<Session, Error> = Session::load_from_string(content);
+        let session = Session::load_from_string(content).unwrap_err();
 
-        assert!(matches!(session, Err(Error::UnableToParseConfig(_))));
+        assert!(matches!(session, Error::UnableToParseConfig(_)));
     }
 
     #[rstest]
-    fn load_from_string_session_file() {
+    fn should_load_from_string() {
         let content = "name: simple-test";
         let session: Session = Session::load_from_string(content).unwrap();
 
@@ -274,6 +390,7 @@ mod tests {
             Some(tmp_dir.to_str().unwrap()),
             || {
                 let result = Session::create(session_name);
+                dbg!(&result);
                 assert!(result.is_ok());
 
                 let created_path = result.unwrap();
@@ -299,13 +416,5 @@ mod tests {
                 );
             },
         );
-    }
-
-    #[rstest]
-    fn when_new_session_invalid_dir() {
-        temp_env::with_var(Session::DEFAULT_DIR_ENV, Some("invalid-dir"), || {
-            let result = Session::create("some-session");
-            assert!(matches!(result, Err(Error::UnableToLoad(_))));
-        });
     }
 }
